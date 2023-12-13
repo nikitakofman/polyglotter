4494400955b8aa9644045bc224e9d75843f3d6df
Nikita <kofman.niki@gmail.com>
2023-12-13 13:06:55 +0100

    fixing design

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="">
       <div className="relative isolate pt-14 dark:bg-gray-900">
-        <div
+        {/* <div
           className="absolute inset-x-0 top-28 -z-10 transofrm-gpu overflow-hidden blur-3xl"
           aria-hidden="true"
         >
@@ -20,23 +20,24 @@ export default async function Home() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
-        </div>
+        </div> */}
         <div className="py-12 sm:py-20 lg:pb-40">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-                Chat with anyone, anywhere!
+                Revolutionizing communication
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
-                You speak your language, they speak their language.{" "}
-                <span className="text-indigo-600 dark:text-indigo-500">
+                Say it in your voice, hear it in theirs – instant multilingual
+                chat made real.
+                {/* <span className="text-indigo-600 dark:text-indigo-500">
                   Let AI handle the translation.
-                </span>
+                </span> */}
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <Link
                   href="/chat"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-[#ef9351] px-3.5 py-2.5 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-[#FE9D52] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
                 </Link>
@@ -44,7 +45,7 @@ export default async function Home() {
                   href="/pricing"
                   className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-300"
                 >
-                  View Pricing <span aria-hidden="true">x</span>
+                  View Pricing <span aria-hidden="true">→</span>
                 </Link>
               </div>
             </div>
@@ -68,7 +69,7 @@ export default async function Home() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80B5] to-[#9089FC] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#FE9D52] to-[#b2a287] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
