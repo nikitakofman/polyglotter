f4c3484a9d8e6d6600e966ec24c64fe38fc3bdf6
Nikita <kofman.niki@gmail.com>
2023-12-16 23:15:38 +0100

    hotfix login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,10 @@
-"use client";
-
 import LoginPage from "@/components/LoginPage";
 import UserButton from "@/components/UserButton";
 import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
 
 function page() {
-  const { data: session } = useSession();
-  return (
-    <div>
-      <LoginPage session={session} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default page;
